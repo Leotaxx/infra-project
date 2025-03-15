1d159c7fe2437b4a6f70b8db53af1b331980160a
Leo Xinyu Yuan <lyleride@gmail.com>
2025-03-15 12:46:26 +0000

    add github actions

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,9 +1,15 @@
 provider "aws" {
   alias  = "primary"
   region = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  session_token=var.aws_session_token
 }
 
 provider "aws" {
   alias  = "secondary"
   region = "us-west-2"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  session_token=var.aws_session_token
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,19 @@ variable "aws_region_primary" { default = "us-east-1" }
 variable "aws_region_secondary" { default = "us-west-2" }
 variable "instance_type" { default = "t3.micro" }
 variable "db_instance_type" { default = "db.t3.medium" }
+variable "aws_access_key" {
+  description = "AWS Access Key"
+  type        = string
+  default     = ""
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Key"
+  type        = string
+  default     = ""
+}
+variable "aws_session_token"{
+    description = "AWS Session token"
+    type        = string
+    default     = ""
+}
