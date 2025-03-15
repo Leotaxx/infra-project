79bbc7076e457d60ec2370380f810002ffae4e3f
Leo Xinyu Yuan <lyleride@gmail.com>
2025-03-15 16:24:58 +0000

    get var from gitaction

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,19 +2,4 @@ variable "aws_region_primary" { default = "us-east-1" }
 variable "aws_region_secondary" { default = "us-west-2" }
 variable "instance_type" { default = "t3.micro" }
 variable "db_instance_type" { default = "db.t3.medium" }
-variable "aws_access_key" {
-  description = "AWS Access Key"
-  type        = string
-  default     = ""
-}
 
-variable "aws_secret_key" {
-  description = "AWS Secret Key"
-  type        = string
-  default     = ""
-}
-variable "aws_secret_session_token"{
-    description = "AWS Session token"
-    type        = string
-    default     = ""
-}
